f67f4b2c604c3a7aa471e35c7b9383274744f59a
codingABI <50620368+codingABI@users.noreply.github.com>
2022-05-15 19:04:39 +0200

    Update GyroRaycaster.ino

--- src/GyroRaycaster.ino
+++ src/GyroRaycaster.ino
@@ -1,6 +1,6 @@
 /*
  * Project: GyroRaycaster
- * Description: Gamelike Raycaster where viewer can be moved and rotated by gyro sensor 
+ * Description: Gamelike raycaster where viewer can be moved and rotated by gyro sensor 
  *   When the viewer has reached the exit to the outer world the games ends
  * Hardward: Arduino Uno/Nano with gyro sensor MPU6050 and SSD1306 OLED 128x64 pixel display
  * License: MIT License
